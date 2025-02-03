0408490b9262d1f4026141426b7349fb1ceaf2ba
Dober09 <lebohanglehutjo@gmail.com>
2025-02-03 18:47:39 +0200

    add main page

---
+++ DiscordClone/ViewModel/MainPage.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Collections.ObjectModel;
+
+namespace DiscordClone.ViewModel
+{
+    class MainPage
+    {
+       
+
+        public ObservableCollection<Model> Models { get; set; }
+
+        public MainPage() { 
+        
+            Models = new ObservableCollection<Model>() { 
+                new Model
+                {
+                    Title = "General",
+                    Icon = "🔨",
+                    subTitle = "Forge",
+                },
+
+            
+            };
+        }
+             
+        
+    }
+
+    public class Model
+    {
+      
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string subTitle { get; set; }
+    }
+}
